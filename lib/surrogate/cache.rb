6e408e88c8cd733d9897adeac426cb899f017be3
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-07-30 15:11:15 -0500

    Basic local-in-memory impl with tests

--- lib/surrogate/cache.rb
+++ lib/surrogate/cache.rb
@@ -2,12 +2,29 @@ require "surrogate/cache/version"
 
 module Surrogate
   class Cache
+
+    #for now implement the cache in-memory as a hash
+    @@cache = Hash.new
+
     def initialize; end
 
-    def get k; end
+    def get k
+      @@cache[k]
+    end
 
-    def set k, v; end
+    def set k, v
+      unless String === k
+        raise ArgumentError.new("Only string keys are supported")
+      end
+      @@cache[k]=v
+    end
 
-    def ping; end
+    def delete k
+      @@cache.delete(k)
+    end
+
+    def ping
+      raise "Not Implemented Yet"
+    end
   end
 end
--- spec/surrogate/cache_spec.rb
+++ spec/surrogate/cache_spec.rb
@@ -1,14 +1,51 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Surrogate::Cache do
-  it 'has a version number' do
+
+  let(:cache){ Surrogate::Cache.new }
+
+  it "has a version number" do
     expect(Surrogate::Cache::VERSION).not_to be nil
   end
 
   context "#initialize" do
     it "should be creatable" do
-      c = Surrogate::Cache.new
-      expect(c).to_not be_nil
+      expect(cache).to_not be_nil
     end
+  end
+
+  context "#set" do
+    it 'should allow you to store a string at a given key' do
+      cache.set "yayyay", "Hooray"
+    end
+    it 'should allow you to store an HTML string at a given key' do
+      cache.set "yayhtml", "<html><body><p>:P</p></body></html>"
+    end
+    it 'should allow you to store a heterogeneous Array at a given key' do
+      cache.set "yayarray", ["a", 2]
+    end
+    it 'should barf on a key that is not a string' do
+      expect{
+        cache.set :im_not_a_string_im_a_symbol, 2
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  context "#get" do
+    before do
+      cache.set "cat", "meow"
+    end
+
+    it "should return nil if key does not store object" do
+      expect(cache.get("unknown")).to be_nil
+    end
+
+    it "should return object if key does store object" do
+      expect(cache.get("cat")).to eq("meow")
+    end
+  end
+
+  context "#ping" do
+    it 'should indicate availability'
   end
 end
