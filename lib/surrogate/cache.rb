4b95c764d4750e356f4dc7d34516ccaec6e505b6
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-07-30 14:07:47 -0500

    Output of 'bundle gem surrogate-cache'

---
+++ lib/surrogate/cache.rb
@@ -0,0 +1,7 @@
+require "surrogate/cache/version"
+
+module Surrogate
+  module Cache
+    # Your code goes here...
+  end
+end
---
+++ lib/surrogate/cache/version.rb
@@ -0,0 +1,5 @@
+module Surrogate
+  module Cache
+    VERSION = "0.0.1"
+  end
+end
